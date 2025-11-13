6009089701b9a23b012030695a15e72b1e87cdc3
Jeisson <jeissoyela@gmail.com>
2025-11-13 11:35:26 -0500

    Feat se creo una calculadora para revelar el area multiplicando el ancho y el alto

--- JeissonYela/jva.js
+++ JeissonYela/jva.js
@@ -1,11 +1,12 @@
 //Calculadora de precios de productos//
-let producto1 = parseFloat(prompt("Ingresa :"));
-let producto2 = parseFloat(prompt("Ingresa el precio del segundo producto:"));
-let producto3 = parseFloat(prompt("Ingresa el precio del tercer producto:"));
-let total = producto1 + producto2 + producto3;
-alert("El total a pagar es: $" + total.toFixed(2));
+let producto1 = parseFloat(prompt("Ingresa precio del primer producto:"))
+let producto2 = parseFloat(prompt("Ingresa precio del segundo producto:"))
+let producto3 = parseFloat(prompt("Ingresa precio del tercer producto:"))
+let total = producto1 + producto2 + producto3
+alert("El total a pagar es: $" + total)
 // //
-let ancho = parseFloat(prompt("Ingresa el ancho de la pared en metros:"));
-let alto = parseFloat(prompt("Ingresa el alto de la pared en metros:"));
-let area = ancho * alto;
-alert("El área de la pared es: " + area.toFixed(2) + " m²");
+let ancho = parseFloat(prompt("Ingresa el ancho de la pared:"))
+let alto = parseFloat(prompt("Ingrese el alto de la pared:"))
+let area = ancho * alto
+alert("El area es: "  + area + " m2")
+
