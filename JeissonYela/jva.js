01501addc9faaa558a317947cf81eb5690f216db
Jeisson <jeissoyela@gmail.com>
2025-11-13 12:11:14 -0500

    feat: saludo formal a cliente}

--- JeissonYela/jva.js
+++ JeissonYela/jva.js
@@ -14,5 +14,17 @@ let largoPotrero = parseFloat(prompt("Ingrese el largo del potrero:"))
 let anchoPotrero = parseFloat(prompt("Ingrese el ancho del potrero:"))
 let perimetro = 2 * (largoPotrero + anchoPotrero)
 alert("El perimetro del potrero es: " + perimetro + " metros")
+//Fahrenheit a Celcius//
+let Fahrenheit = parseFloat(prompt("Ingrese los grados Fahrenheit:"))
+let Celcius = (Fahrenheit - 32)+ 5/9
+alert("La temperatura en grados Celcius es " + Celcius + " C°")
+ //Calculadora de Edad//
+ let AñoDeNacimiento = parseFloat(prompt("Ingrese su año de nacimiento"))
+ let AñoActual = 2024
+ let Edad = AñoActual - AñoDeNacimiento 
+ alert("Su edad es " + Edad)
+//Saludo//
+let Nombre = (prompt("Ingrese su nombre"))
+let Apellido = (prompt("Ingrese su Apellido"))
 
-
+alert("Bienvenido " + Nombre +" "+ Apellido + "Disfrute su visita :)")
