a35adc3f0666af63fd9002c1a680e3cc8d5c6e11
Jeisson <jeissoyela@gmail.com>
2025-11-13 11:56:37 -0500

    feat: convertir temperatura de Fahrenheit a Celsius

--- JeissonYela/jva.js
+++ JeissonYela/jva.js
@@ -4,9 +4,15 @@ let producto2 = parseFloat(prompt("Ingresa precio del segundo producto:"))
 let producto3 = parseFloat(prompt("Ingresa precio del tercer producto:"))
 let total = producto1 + producto2 + producto3
 alert("El total a pagar es: $" + total)
-// //
+//Calculadora de Area //
 let ancho = parseFloat(prompt("Ingresa el ancho de la pared:"))
 let alto = parseFloat(prompt("Ingrese el alto de la pared:"))
 let area = ancho * alto
 alert("El area es: "  + area + " m2")
+//Calculadora de perimetro //
+let largoPotrero = parseFloat(prompt("Ingrese el largo del potrero:"))
+let anchoPotrero = parseFloat(prompt("Ingrese el ancho del potrero:"))
+let perimetro = 2 * (largoPotrero + anchoPotrero)
+alert("El perimetro del potrero es: " + perimetro + " metros")
+
 
